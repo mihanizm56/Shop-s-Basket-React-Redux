b09e852a8a3f6d7fd1f168c7737c58bcf00e7c18
mihanizm56 <m727507.56@mail.ru>
2018-12-22 01:43:05 +0400

    fixed tests of Store

--- src/Store/index.test.js
+++ src/Store/index.test.js
@@ -1,97 +1,201 @@
-import { createStore } from "redux"
 import { store } from "../Store/"
+import {
+  incrementItem,
+  decrementItem,
+  deleteItem,
+  checkDiscount,
+} from "../Actions/"
 
 describe("Store testing", () => {
-  it("Makes correct store", () => {
-    const testInitialState = {
-      userName: "Анастасия",
-      promocode: "123",
-      discount: false,
-      discountValue: 1800,
-      adItems: [
-        {
-          img: 1,
-          name: "Солнечные очки зеленого цвета в стиле ретро",
-          price: "2450",
-        },
-        {
-          img: 2,
-          name: "Шляпа",
-          price: "800",
-        },
-        {
-          img: 3,
-          name: "Платье-миди с расклешенной юбкой",
-          price: "2450",
-        },
-        {
-          img: 4,
-          name: "Платье-миди с расклешенной юбкой",
-          price: "2450",
-        },
-      ],
-      userItems: [
-        {
-          img: "./img/img1.png",
-          description: "Платье-миди с расклешенной юбкой",
-          code: "82039-11",
-          size: "44",
-          color: "синий",
-          price: 2450,
-          numberOfItems: 1,
-          id: 1,
-        },
-        {
-          img: "./img/img2.png",
-          description: "Туфли женские украшенные кружевными вставками",
-          code: "13524-01",
-          size: "38",
-          color: "черный",
-          price: 2450,
-          numberOfItems: 1,
-          id: 2,
-        },
-        {
-          img: "./img/img3.png",
-          description: "Платье-миди",
-          code: "75039",
-          size: "44",
-          color: "белый",
-          price: 2450,
-          numberOfItems: 1,
-          id: 3,
-        },
-        {
-          img: "./img/img4.png",
-          description: "Платье-миди",
-          code: "75039",
-          size: "46",
-          color: "белый",
-          price: 2450,
-          numberOfItems: 1,
-          id: 4,
-        },
-      ],
-    }
+  const initState = {
+    adItems: [
+      {
+        img: 1,
+        name: "Солнечные очки зеленого цвета в стиле ретро",
+        price: "2450",
+      },
+      { img: 2, name: "Шляпа", price: "800" },
+      { img: 3, name: "Платье-миди с расклешенной юбкой", price: "2450" },
+      { img: 4, name: "Платье-миди с расклешенной юбкой", price: "2450" },
+    ],
+    discount: false,
+    discountValue: 1800,
+    promocode: "123",
+    userItems: [
+      {
+        code: "82039-11",
+        color: "синий",
+        description: "Платье-миди с расклешенной юбкой",
+        id: 1,
+        img: "./img/img1.png",
+        numberOfItems: 1,
+        price: 2450,
+        size: "44",
+      },
+      {
+        code: "13524-01",
+        color: "черный",
+        description: "Туфли женские украшенные кружевными вставками",
+        id: 2,
+        img: "./img/img2.png",
+        numberOfItems: 1,
+        price: 2450,
+        size: "38",
+      },
+      {
+        code: "75039",
+        color: "белый",
+        description: "Платье-миди",
+        id: 3,
+        img: "./img/img3.png",
+        numberOfItems: 1,
+        price: 2450,
+        size: "44",
+      },
+      {
+        code: "75039",
+        color: "белый",
+        description: "Платье-миди",
+        id: 4,
+        img: "./img/img4.png",
+        numberOfItems: 1,
+        price: 2450,
+        size: "46",
+      },
+    ],
+    userName: "Анастасия",
+  }
 
-    const testReducer = (state = testInitialState, action) => {
-      switch (action.type) {
-        case "INCREMENT_ITEM":
-          return { ...state, userItems: action.payload }
-        case "DECREMENT_ITEM":
-          return { ...state, userItems: action.payload }
-        case "DELETE_ITEM":
-          return { ...state, userItems: action.payload }
-        case "CHECK_DISCOUNT":
-          return { ...state, discount: action.payload }
+  const incrementState = {
+    adItems: [
+      {
+        img: 1,
+        name: "Солнечные очки зеленого цвета в стиле ретро",
+        price: "2450",
+      },
+      { img: 2, name: "Шляпа", price: "800" },
+      { img: 3, name: "Платье-миди с расклешенной юбкой", price: "2450" },
+      { img: 4, name: "Платье-миди с расклешенной юбкой", price: "2450" },
+    ],
+    discount: false,
+    discountValue: 1800,
+    promocode: "123",
+    userItems: [
+      {
+        code: "82039-11",
+        color: "синий",
+        description: "Платье-миди с расклешенной юбкой",
+        id: 1,
+        img: "./img/img1.png",
+        numberOfItems: 1,
+        price: 2450,
+        size: "44",
+      },
+      {
+        code: "13524-01",
+        color: "черный",
+        description: "Туфли женские украшенные кружевными вставками",
+        id: 2,
+        img: "./img/img2.png",
+        numberOfItems: 2,
+        price: 2450,
+        size: "38",
+      },
+      {
+        code: "75039",
+        color: "белый",
+        description: "Платье-миди",
+        id: 3,
+        img: "./img/img3.png",
+        numberOfItems: 1,
+        price: 2450,
+        size: "44",
+      },
+      {
+        code: "75039",
+        color: "белый",
+        description: "Платье-миди",
+        id: 4,
+        img: "./img/img4.png",
+        numberOfItems: 1,
+        price: 2450,
+        size: "46",
+      },
+    ],
+    userName: "Анастасия",
+  }
 
-        default:
-          return state
-      }
-    }
+  const deletedState = {
+    adItems: [
+      {
+        img: 1,
+        name: "Солнечные очки зеленого цвета в стиле ретро",
+        price: "2450",
+      },
+      { img: 2, name: "Шляпа", price: "800" },
+      { img: 3, name: "Платье-миди с расклешенной юбкой", price: "2450" },
+      { img: 4, name: "Платье-миди с расклешенной юбкой", price: "2450" },
+    ],
+    discount: false,
+    discountValue: 1800,
+    promocode: "123",
+    userItems: [
+      {
+        code: "82039-11",
+        color: "синий",
+        description: "Платье-миди с расклешенной юбкой",
+        id: 1,
+        img: "./img/img1.png",
+        numberOfItems: 1,
+        price: 2450,
+        size: "44",
+      },
+      {
+        code: "75039",
+        color: "белый",
+        description: "Платье-миди",
+        id: 3,
+        img: "./img/img3.png",
+        numberOfItems: 1,
+        price: 2450,
+        size: "44",
+      },
+      {
+        code: "75039",
+        color: "белый",
+        description: "Платье-миди",
+        id: 4,
+        img: "./img/img4.png",
+        numberOfItems: 1,
+        price: 2450,
+        size: "46",
+      },
+    ],
+    userName: "Анастасия",
+  }
 
-    const testStore = createStore(testReducer)
+  const id = 2
+  const indexOfItem = 1
 
-    expect(typeof testStore).toBe(typeof store)
+  it("Makes correct initState", () => {
+    expect(store.getState()).toEqual(initState)
+  })
+  it("Makes correct incrementItem", () => {
+    store.dispatch(incrementItem(initState.userItems, id, indexOfItem))
+    expect(store.getState()).toEqual(incrementState)
+  })
+  it("Makes correct decrementItem", () => {
+    store.dispatch(decrementItem(incrementState.userItems, id, indexOfItem))
+    expect(store.getState()).toEqual(initState)
+  })
+
+  it("Makes correct decrementItem", () => {
+    store.dispatch(deleteItem(initState.userItems, id))
+    expect(store.getState()).toEqual(deletedState)
+  })
+
+  it("Makes correct decrementItem", () => {
+    store.dispatch(checkDiscount(incrementState.promocode, "123"))
+    expect(store.getState().discount).toBe(true)
   })
 })
