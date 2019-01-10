ef1767ae56a6981ccff0f951afe318e6bdf8e35f
Michael_Kozhevnikov <mkozhevnikov@technoserv.com>
2019-01-10 12:35:34 +0300

    travis added

---
+++ build/precache-manifest.930018584e60e035487c7a156c5d5cfb.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "90ea4755814ad8644bc3e34101993180",
+    "url": "/static/media/item1.90ea4755.png"
+  },
+  {
+    "revision": "eea21f814ab736076107",
+    "url": "/static/css/main.e613994c.chunk.css"
+  },
+  {
+    "revision": "51ace446331ba7771c87",
+    "url": "/static/js/1.51ace446.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8a1325f1ee433884cd3766959ef2ac57",
+    "url": "/static/media/good-1.8a1325f1.png"
+  },
+  {
+    "revision": "29bf0d9270a7f0e9d2720258d8a31494",
+    "url": "/static/media/good-2.29bf0d92.png"
+  },
+  {
+    "revision": "6d2c134839faa79f6ff67d799def6169",
+    "url": "/static/media/good-3.6d2c1348.png"
+  },
+  {
+    "revision": "4ba66ad205258dbdaf9a53824aa5ebbd",
+    "url": "/static/media/good-4.4ba66ad2.png"
+  },
+  {
+    "revision": "eea21f814ab736076107",
+    "url": "/static/js/main.eea21f81.chunk.js"
+  },
+  {
+    "revision": "3c1ae6da12328e8daeaa64148604fe5b",
+    "url": "/static/media/item2.3c1ae6da.png"
+  },
+  {
+    "revision": "1f243e3f0f5abd469f07f6c2daeb2de8",
+    "url": "/static/media/item3.1f243e3f.png"
+  },
+  {
+    "revision": "afa7a91dadd77b23634a0fdf18c148f3",
+    "url": "/static/media/Roboto-Bold.afa7a91d.ttf"
+  },
+  {
+    "revision": "54a91b0619ccf9373d525109268219dc",
+    "url": "/static/media/Roboto-Regular.54a91b06.ttf"
+  },
+  {
+    "revision": "99fc0816a09395454061301fefa42bf1",
+    "url": "/static/media/Roboto-Medium.99fc0816.ttf"
+  },
+  {
+    "revision": "5ecbc99d1a81fed7dc71cb068ec0a74d",
+    "url": "/static/media/Roboto-Thin.5ecbc99d.ttf"
+  },
+  {
+    "revision": "da16cc596750fe55e206fa6146b23f6a",
+    "url": "/index.html"
+  }
+];
